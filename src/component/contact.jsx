1edbb6a482d0e633a6a511ce015b0bf19950e6c2
Abiodun-34 <1abiodun34@gmail.com>
2025-07-17 17:58:04 +0100

    Add files via upload

--- src/component/contact.jsx
+++ src/component/contact.jsx
@@ -23,7 +23,7 @@ const Contact = () => {
   return (
     // Added pb-16 to create space below the contact section.
     // Removed mb-0 to allow for default margins or further spacing if needed.
-    <section className="bg-gray-800 pb-16 min-h-[calc(100vh-80px)]">
+    <section className="bg-blue-300 pb-16 min-h-[calc(100vh-80px)]">
       {/* Main container with equal columns */}
       <div className="flex flex-col lg:flex-row justify-between gap-8 w-full max-w-7xl mx-auto pt-8">
         
